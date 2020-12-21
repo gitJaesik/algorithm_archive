acce41f82ef85db3c39d4dbce21d758cfc8de78a
gitJaesik <maguire1815@gmail.com>
2020-12-21 14:44:15 +0900

    Update pro

--- codility/count_distinct_slices.cpp
+++ codility/count_distinct_slices.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <unordered_map>
 
+// 1000000000
+// 705082704
 using namespace std;
 
 int solution(int M, vector<int> &A) {
@@ -17,18 +19,21 @@ int solution(int M, vector<int> &A) {
 
     for (int back = 0; back < N; ++back) {
         while (front < N) {
+            // 이미 존재하는 경우 / non distinct
             if (h.find(A[front]) != h.end()) {
-                front -= 1;
                 break;
             }
-            h[A[front]] = 1;
-            front +=1;
-            total +=1;
+            else {
+                h[A[front]] = 1;
+                front +=1;
+                total +=1;
+            }
         }
 
         ans += total;
         h.erase(A[back]);
         total -= 1;
+        if (ans > 1000000000) return 1000000000;
     }
 
     return ans;
@@ -37,8 +42,34 @@ int solution(int M, vector<int> &A) {
 
 int main() {
 
-    vector<int> A {3, 4, 5, 5, 2};
+    // vector<int> A {3, 4, 5, 5, 2};
+    // vector<int> A {1, 2, 1, 2, 1};
+    /*
+    1,
+    1, 2,
+    2,
+    2, 1,
+    1,
+    1, 2,
+    2,
+    2, 1,
+    1
+    */
+    vector<int> A {1, 2, 1, 2};
+    /*
+    1,
+    1, 2,
+    2,
+    2, 1,
+    1,
+    1, 2,
+    2,
+    */
+
+
     cout << solution(6, A) << endl;
 
     return 0;
 }
+
+// https://app.codility.com/demo/results/training2QMHZA-HJG/
