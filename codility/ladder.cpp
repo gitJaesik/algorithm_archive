a06958efc3d70427720bafbfcf6b3da1c909ae25
gitJaesik <maguire1815@gmail.com>
2020-12-22 11:31:28 +0900

    Update pro : module time out

--- codility/ladder.cpp
+++ codility/ladder.cpp
@@ -9,37 +9,25 @@ vector<int> solution(vector<int> &A, vector<int> &B) {
 
     int MAX_L = 500002;
 
-    // long long fibo[MAX_L];
-    // fibo[0] = 0;
-    // fibo[1] = 1;
-    // for (int i = 2; i < MAX_L; ++i) {
-    //     fibo[i] = fibo[i - 1] + fibo[i - 2];
-    // }
-
     long long pow_of_two[31];
     pow_of_two[0] = 1;
     for (int i = 1; i < 31; ++i) {
         pow_of_two[i] = pow_of_two[i-1] * 2;
     }
 
+    int fibo[MAX_L];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i < MAX_L; ++i) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+        fibo[i] = fibo[i] % pow_of_two[30];
+    }
+
     // 2 ^ 30 = 1073741824 // integer
     int N = A.size();
     for (int i = 0; i < N; ++i) {
-        // zero base
-        long long long_ans = 0;
-        long long long_ans_prev = 1;
-        long long long_ans_before_prev = 0;
-        for (int j = 1; j <= A[i]; ++j) {
-            long_ans = ((long_ans_prev % pow_of_two[B[i]]) + (long_ans_before_prev % pow_of_two[B[i]])) % pow_of_two[B[i]];
-            long_ans_before_prev = long_ans_prev % pow_of_two[B[i]];
-            long_ans_prev = long_ans % pow_of_two[B[i]];
-        }
-
-        //  fibo[A[i] + 1] % pow_of_two[B[i]];
-        int int_ans = long_ans;
-        ans.push_back(int_ans);
+        ans.push_back(fibo[A[i] + 1] % pow_of_two[B[i]]);
     }
-    // https://app.codility.com/demo/results/trainingM7ZWC8-8X8/
 
     return ans;
 }
