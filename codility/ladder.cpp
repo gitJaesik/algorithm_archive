ed6ddac824956f60437093a0d2b366b0c0f91235
gitJaesik <maguire1815@gmail.com>
2020-12-22 11:27:05 +0900

    Update pro: overflow

--- codility/ladder.cpp
+++ codility/ladder.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
 vector<int> solution(vector<int> &A, vector<int> &B) {
     vector<int> ans;
 
-    int MAX_L = 500001;
+    int MAX_L = 500002;
 
-    int fibo[MAX_L];
-    fibo[0] = 0;
-    fibo[1] = 1;
-    for (int i = 2; i < MAX_L; ++i) {
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    // long long fibo[MAX_L];
+    // fibo[0] = 0;
+    // fibo[1] = 1;
+    // for (int i = 2; i < MAX_L; ++i) {
+    //     fibo[i] = fibo[i - 1] + fibo[i - 2];
+    // }
+
+    long long pow_of_two[31];
+    pow_of_two[0] = 1;
+    for (int i = 1; i < 31; ++i) {
+        pow_of_two[i] = pow_of_two[i-1] * 2;
     }
 
     // 2 ^ 30 = 1073741824 // integer
     int N = A.size();
     for (int i = 0; i < N; ++i) {
         // zero base
-        ans.push_back(fibo[A[i] + 1] % (int)(pow(2, B[i])));
+        long long long_ans = 0;
+        long long long_ans_prev = 1;
+        long long long_ans_before_prev = 0;
+        for (int j = 1; j <= A[i]; ++j) {
+            long_ans = ((long_ans_prev % pow_of_two[B[i]]) + (long_ans_before_prev % pow_of_two[B[i]])) % pow_of_two[B[i]];
+            long_ans_before_prev = long_ans_prev % pow_of_two[B[i]];
+            long_ans_prev = long_ans % pow_of_two[B[i]];
+        }
+
+        //  fibo[A[i] + 1] % pow_of_two[B[i]];
+        int int_ans = long_ans;
+        ans.push_back(int_ans);
     }
+    // https://app.codility.com/demo/results/trainingM7ZWC8-8X8/
 
     return ans;
 }
