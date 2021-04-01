7050812aa185fb68d3a8bf0006c900602504cf45
gitJaesik <maguire1815@gmail.com>
2021-04-01 19:29:06 +0900

    Golang algorithm first commit

---
+++ acmicpc/golang/golang_guide.go
@@ -0,0 +1,11 @@
+// +build ignore
+
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scanf("%d %d", &a, &b)
+	fmt.Printf("%d\n", a+b)
+}
---
+++ acmicpc/golang/main.go
@@ -0,0 +1,14 @@
+// 10171
+
+package main
+
+import "fmt"
+
+func main() {
+	str := `\    /\
+ )  ( ')
+(  /  )
+ \(__)|`
+
+	fmt.Printf(str)
+}
